d474a6a24ea8d4f2a9a69c5a499c71203a7c7315
hamster1980 <79819721701@yandex,ru>
2014-04-06 15:33:38 +0400

    todo

---
+++ checkstyle/src/main/java/com/hamster/checkstyle/ExcludeRuleDecorator.java
@@ -0,0 +1,10 @@
+package com.hamster.checkstyle;
+
+import java.util.Collection;
+
+public class ExcludeRuleDecorator {
+    /*
+     * 1. exclude duplicate lines for com.hamster.model package
+     */
+    private Collection<String> excludePackages;
+}
